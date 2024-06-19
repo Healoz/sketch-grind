a4a9ae83d7090499ca101c2c36156fb1d026538d
Healoz <lauren.easter59@gmail.com>
2024-06-19 10:28:22 +1000

    seperated session setup into its own component

---
+++ app/components/SessionSetup.tsx
@@ -0,0 +1,59 @@
+import React, { FC } from 'react'
+import { AnimatePresence, motion } from "framer-motion";
+import UploadImage from './image-upload/UploadImage';
+import MainHeading from './MainHeading';
+import ImagePreview from './image-upload/ImagePreview';
+
+interface SessionSetupProps {
+    preview: string | ArrayBuffer | null;
+    setPreview: (preview: string | ArrayBuffer | null) => void;
+}
+
+const SessionSetup: FC<SessionSetupProps> = ({preview, setPreview}) => {
+
+  return (
+    <div>
+          <MainHeading />
+          <AnimatePresence>
+            {preview ? (
+              <motion.div
+                key={1}
+                initial={{ opacity: 0, x: 600, y: 50, scale: 1 }}
+                animate={{
+                  opacity: 1,
+                  x: 0,
+                  y: 0,
+                  scale: 1,
+                  transition: { delay: 0.31 },
+                }}
+                exit={{ opacity: 0, scale: 0, rotate: 180, x: -100, y: -100 }}
+                transition={{
+                  duration: 0.3,
+                }}
+              >
+                <ImagePreview preview={preview} setPreview={setPreview} />
+              </motion.div>
+            ) : (
+              <motion.div
+                key={2}
+                initial={{ opacity: 0, x: -600, y: -50 }}
+                animate={{
+                  opacity: 1,
+                  x: 0,
+                  y: 0,
+                  transition: { delay: 0.31 },
+                }}
+                exit={{ opacity: 0, x: -600, y: -50 }}
+                transition={{
+                  duration: 0.3,
+                }}
+              >
+                <UploadImage setPreview={setPreview} />
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+  )
+}
+
+export default SessionSetup
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,11 @@ import UploadImage from "./components/image-upload/UploadImage";
 import MainHeading from "./components/MainHeading";
 import ImagePreview from "./components/image-upload/ImagePreview";
 import { useState } from "react";
+import SessionSetup from "./components/SessionSetup";
 
 export default function Home() {
   const [preview, setPreview] = useState<string | ArrayBuffer | null>(null);
+  const [sessionStarted, setSessionStarted] = useState(false);
 
   return (
     <main
@@ -14,38 +16,11 @@ export default function Home() {
       // style={{ backgroundImage: preview ? `url(${preview})` : "none"
     >
       <motion.div className="min-w-full flex flex-col h-svh p-10" layout>
-        <MainHeading />
-        <AnimatePresence>
-          {preview ? (
-            <motion.div
-              key={1}
-              initial={{ opacity: 0, x: 600, y: 50 }}
-              animate={{ opacity: 1, x: 0, y: 0 }}
-              exit={{ opacity: 0, x: 600, y: 50 }}
-              transition={{
-                delay: 0.73,
-                duration: 0.7,
-                type: "spring",
-              }}
-            >
-              <ImagePreview preview={preview} setPreview={setPreview} />
-            </motion.div>
-          ) : (
-            <motion.div
-              key={2}
-              initial={{ opacity: 0, x: -600, y: -50 }}
-              animate={{ opacity: 1, x: 0, y: 0 }}
-              exit={{ opacity: 0, x: -600, y: -50 }}
-              transition={{
-                delay: preview ? 0.73 : 0, // Apply delay only when transitioning from on to off
-                duration: 0.7,
-                type: "spring",
-              }}
-            >
-              <UploadImage setPreview={setPreview} />
-            </motion.div>
-          )}
-        </AnimatePresence>
+        {!sessionStarted ? (
+          <SessionSetup preview={preview} setPreview={setPreview}/>
+        ) : (
+          <h1>Hello</h1>
+        )}
       </motion.div>
     </main>
   );
