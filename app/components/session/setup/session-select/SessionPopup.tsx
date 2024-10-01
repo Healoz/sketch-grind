a9073d2637964958d61a761e6b4b2e9f81a560a9
Healoz <lauren.easter59@gmail.com>
2024-10-01 19:39:52 +1000

    popup opens and closes

--- app/components/session/setup/session-select/SessionPopup.tsx
+++ app/components/session/setup/session-select/SessionPopup.tsx
@@ -1,5 +1,8 @@
+import Button from "@/app/components/Button";
 import { Session } from "@/app/types/Session";
+import { Size } from "@/app/types/Size";
 import useClickOutside from "@/app/utils/useClickOutside";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 import React, { FC, useRef } from "react";
 
 interface SessionPopupProps {
@@ -18,8 +21,17 @@ const SessionPopup: FC<SessionPopupProps> = ({
 
   return (
     <div className="absolute w-full h-full top-0 bottom-0 flex items-center justify-center">
-      <div className="bg-red-500 p-5" ref={sessionPopupRef}>
-        {session.name}
+      <div
+        className="bg-slate-900 p-5 border rounded border-pink-200 w-1/2"
+        ref={sessionPopupRef}
+      >
+        <div className="flex justify-between">
+          <h2 className="text-2xl">{session.name}</h2>
+          <Button icon={CloseRoundedIcon} size={Size.SMALL} onClick={() => setSessionPopupShown(false)} />
+        </div>
+
+        <p>Total time: </p>
+        <p>Steps:</p>
       </div>
     </div>
   );
